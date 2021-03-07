282ae78fd5c03fffa9469f82aefd169868283806
anweshadas <anwesha@das.community>
2021-03-07 22:23:17 +0530

    Adds initial code for downloading file

---
+++ day37/download.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os 
+import argparse
+import httpx
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="input filepath",
+                    type=str)
+args = parser.parse_args()
+
+filename = args.input
+
+with open(filename) as fobj:
+    for line in fobj:
+        url = line.strip()
+        r = httpx.get(url)
+        fname = url.split("/")[-1]
+        print(f"downloading {fname}")
+        with open(fname,"wb") as fobj:
+            fobj.write(r.read())
