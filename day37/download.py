70faeb39f49e3fbe98a04fe4c66a667d2ecb99e3
anweshadas <anwesha@das.community>
2021-03-12 13:09:19 +0530

    Adds the code for taking directory as input

--- day37/download.py
+++ day37/download.py
@@ -9,15 +9,23 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="input filepath",
                         type=str, required=True)
+    parser.add_argument("--output", help="output directory",
+                        type=str)                        
+    
     args = parser.parse_args()
 
     filename = args.input
+    if args.output:
+        directory = args.output
+    else:
+        directory = "./"
 
     with open(filename) as fobj:
         for line in fobj:
             url = line.strip()
             r = httpx.get(url)
             fname = url.split("/")[-1]
+            fname = os.path.join(directory,fname)
             print(f"Downloading {fname}")
             with open(fname,"wb") as fobj:
                 fobj.write(r.read())
