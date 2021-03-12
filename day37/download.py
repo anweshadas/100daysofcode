670155e6283353143776ed3ff08702a730bc8c49
anweshadas <anwesha@das.community>
2021-03-12 10:40:33 +0530

    Adds main function to the code

--- day37/download.py
+++ day37/download.py
@@ -5,18 +5,22 @@ import argparse
 import httpx
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input", help="input filepath",
-                    type=str, required=True)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", help="input filepath",
+                        type=str, required=True)
+    args = parser.parse_args()
 
-filename = args.input
+    filename = args.input
 
-with open(filename) as fobj:
-    for line in fobj:
-        url = line.strip()
-        r = httpx.get(url)
-        fname = url.split("/")[-1]
-        print(f"downloading {fname}")
-        with open(fname,"wb") as fobj:
-            fobj.write(r.read())
+    with open(filename) as fobj:
+        for line in fobj:
+            url = line.strip()
+            r = httpx.get(url)
+            fname = url.split("/")[-1]
+            print(f"Downloading {fname}")
+            with open(fname,"wb") as fobj:
+                fobj.write(r.read())
+
+if __name__ == "__main__":
+    main()
